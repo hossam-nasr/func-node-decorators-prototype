257e58c5b0731d090cb732e5d9b99a62e6f4a491
hossam-nasr <hossam.nasr99@gmail.com>
2022-10-21 20:38:16 -0700

    add post and get helpers

--- framework/http.ts
+++ framework/http.ts
@@ -4,10 +4,31 @@ import FunctionApp from "./FunctionApp";
 export function http(options: HttpTriggerOptions = {}) {
     return function (target: any, propertyKey: string | symbol, index: number) {
         let functionName = propertyKey.toString();
-        const httpOptions = { index, ...azFuncTrigger.http(options) };
-        FunctionApp.addTrigger(functionName, httpOptions);
-        if (!FunctionApp.hasReturnValue(functionName)) {
-            FunctionApp.setReturnValue(functionName, AzFuncOutput.http({}));
-        }
+        addHttpTrigger(functionName, options, index);
     };
+}
+
+export function httpGet(options: HttpTriggerOptions = {}) {
+    return function (target: any, propertyKey: string | symbol, index: number) {
+        let functionName = propertyKey.toString();
+        options.methods ||= ['GET'];
+        addHttpTrigger(functionName, options, index);
+    };
+}
+
+export function httpPost(options: HttpTriggerOptions = {}) {
+    return function (target: any, propertyKey: string | symbol, index: number) {
+        let functionName = propertyKey.toString();
+        options.methods ||= ['POST'];
+        addHttpTrigger(functionName, options, index);
+    };
+}
+
+
+function addHttpTrigger (functionName: string, httpOptions: HttpTriggerOptions, index: number): void {
+    const triggerOptions = { index, ...azFuncTrigger.http(httpOptions) };
+    FunctionApp.addTrigger(functionName, triggerOptions);
+    if (!FunctionApp.hasReturnValue(functionName)) {
+        FunctionApp.setReturnValue(functionName, AzFuncOutput.http({}));
+    }
 }
